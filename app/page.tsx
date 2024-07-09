1eb9cfb7c14d1ddcfe0fb76d3d98349ad8bfcc6c
Nelson <tomneo2004@gmail.com>
2024-07-09 12:42:43 +0800

    banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Banner from "@/components/banner.client";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Coffee Connoisseur</h1>
+      <Banner />
     </main>
   );
 }
