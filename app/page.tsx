271d87fab53189f5aa983e00e563d43006bc62a5
Nelson <tomneo2004@gmail.com>
2024-07-18 10:59:06 +0800

    coffee-store dynamic route

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Banner from "@/components/banner.client";
+import Link from "next/link";
 
 export default function Home() {
+  const coffeeStoreId = "dark-horse-coffee";
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="mx-auto mt-10 max-w-6xl px-4">
       <Banner />
+      <Link href={`coffee-store/${coffeeStoreId}`}>Dark horse coffee</Link>
     </main>
   );
 }
